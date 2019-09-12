fd301532d251246ef23f3b9c5669c886a232fb40
Paolo Pironi <ppiron@gmail.com>
2019-09-12 11:25:17 +0100

    part2.13

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios'
 
+const Country = ({ country }) => (
+  <>
+    <h2>{country.name}</h2>
+    <p>capital {country.capital}</p>
+    <p>population {country.population}</p>
+    <h3>languages</h3>
+    <ul>
+      {country.languages.map(language => (
+        <li key={language.iso639_1}>{language.name}</li>
+      ))}
+    </ul>
+    <img width="200px" src={country.flag} alt={`flag of ${country.name}`} />
+  </>
+)
+
 function App() {
   const [query, setQuery] = useState('')
   const [countries, setCountries] = useState([])
+  const [showCountry, setShowCountry] = useState([])
 
   const onQueryChange = event => {
     setQuery(event.target.value)
@@ -14,11 +30,19 @@ function App() {
           // console.log(response.data.length)
           if (response.data.length <= 10) {
             setCountries(response.data)
+            setShowCountry(Array.from({ length: response.data.length }, (v, i) => 0))
           } else {
             setCountries([])
+            setShowCountry([])
           }
         })
     }
+  }
+
+  const toggleShowCountry = (ix) => {
+    // console.log(showCountry)
+    // console.log(ix)
+    showCountry[ix] === 0 ? setShowCountry(showCountry.slice(0, ix).concat(1).concat(...showCountry.slice(ix + 1))) : setShowCountry(showCountry.slice(0, ix).concat(0).concat(...showCountry.slice(ix + 1)))
   }
 
   return (
@@ -28,21 +52,19 @@ function App() {
         countries.length === 0 ?
           <p>Too many matches, specify another filter</p> :
           countries.length > 1 ?
-            countries.map(country => {
-              return <p key={country.alpha2Code}>{country.name}</p>
-            }) :
-            <>
-              <h2>{countries[0].name}</h2>
-              <p>capital {countries[0].capital}</p>
-              <p>population {countries[0].population}</p>
-              <h3>languages</h3>
-              <ul>
-                {countries[0].languages.map(language => (
-                  <li key={language.iso639_1}>{language.name}</li>
-                ))}
-              </ul>
-              <img width="200px" src={countries[0].flag} alt={`flag of ${countries[0].name}`} />
-            </>
+            countries.map((country, i) => {
+              return (
+                <React.Fragment key={country.alpha2Code}>
+                  <p>
+                    {country.name}
+                    &nbsp;<button onClick={event => toggleShowCountry(i)}>
+                      {showCountry[i] === 1 ? 'hide' : 'show'}
+                    </button>
+                  </p>
+                  {showCountry[i] === 1 ? <Country country={countries[i]} /> : null}
+                </ React.Fragment>
+              )
+            }) : <Country country={countries[0]} />
       }
     </>
   )
